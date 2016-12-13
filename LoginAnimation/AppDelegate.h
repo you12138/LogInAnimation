ec2b83820dc9f13eafb044480f6eb775e838aafb
whn <whn@whntekiiMac.local>
2016-12-13 17:46:42 +0800

    Initial Commit

---
+++ LoginAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginAnimation
+//
+//  Created by whn on 2016/12/13.
+//  Copyright © 2016年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginAnimation
+//
+//  Created by whn on 2016/12/13.
+//  Copyright © 2016年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
